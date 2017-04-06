da2f634dd669e6a29e37560b288cda1d96a42bd6
Artem Novikov <novikov@i-market.ru>
2017-04-06 13:23:22 +0300

    whitespace

--- public/index.php
+++ public/index.php
@@ -10,290 +10,290 @@ $APPLICATION->SetTitle("Грумелье");
 ?>
 
 <?= HeroBanner::render(HeroBanner::HOMEPAGE_CODE); ?>
-<section class="service" data-anchor="next">
-    <div class="left wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0s"></div>
-    <div class="right wow fadeInRight" data-wow-duration="1s" data-wow-delay="0s"></div>
-    <div class="wrap">
-        <strong class="heading">
-            <? $APPLICATION->IncludeComponent(
-            	"bitrix:main.include",
-            	"",
-            	Array(
-            		"AREA_FILE_SHOW" => "file",
-            		"PATH" => v::includedArea('homepage/service_heading.php')
-            	)
-            ); ?>
-        </strong>
-        <? $APPLICATION->IncludeComponent(
-        	"bitrix:news.list",
-        	"service",
-        	Array(
-        		"ACTIVE_DATE_FORMAT" => "j F Y",
-        		"ADD_SECTIONS_CHAIN" => "N",
-        		"AJAX_MODE" => "N",
-        		"AJAX_OPTION_ADDITIONAL" => "",
-        		"AJAX_OPTION_HISTORY" => "N",
-        		"AJAX_OPTION_JUMP" => "N",
-        		"AJAX_OPTION_STYLE" => "Y",
-        		"CACHE_FILTER" => "N",
-        		"CACHE_GROUPS" => "Y",
-        		"CACHE_TIME" => "36000000",
-        		"CACHE_TYPE" => "A",
-        		"CHECK_DATES" => "Y",
-        		"DETAIL_URL" => "",
-        		"DISPLAY_BOTTOM_PAGER" => "Y",
-        		"DISPLAY_DATE" => "Y",
-        		"DISPLAY_NAME" => "Y",
-        		"DISPLAY_PICTURE" => "Y",
-        		"DISPLAY_PREVIEW_TEXT" => "Y",
-        		"DISPLAY_TOP_PAGER" => "N",
-        		"FIELD_CODE" => array("", ""),
-        		"FILTER_NAME" => "",
-        		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-        		"IBLOCK_ID" => IblockTools::find(Iblock::CONTENT_TYPE, Iblock::SERVICE)->id(),
-        		"IBLOCK_TYPE" => Iblock::CONTENT_TYPE,
-        		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-        		"INCLUDE_SUBSECTIONS" => "Y",
-        		"MESSAGE_404" => "",
-        		"NEWS_COUNT" => PHP_INT_MAX,
-        		"PAGER_BASE_LINK_ENABLE" => "N",
-        		"PAGER_DESC_NUMBERING" => "N",
-        		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-        		"PAGER_SHOW_ALL" => "N",
-        		"PAGER_SHOW_ALWAYS" => "N",
-        		"PAGER_TEMPLATE" => ".default",
-        		"PAGER_TITLE" => '',
-        		"PARENT_SECTION" => "",
-        		"PARENT_SECTION_CODE" => "",
-        		"PREVIEW_TRUNCATE_LEN" => "",
-        		"PROPERTY_CODE" => array("LINK", "ICON"),
-        		"SET_BROWSER_TITLE" => "N",
-        		"SET_LAST_MODIFIED" => "N",
-        		"SET_META_DESCRIPTION" => "N",
-        		"SET_META_KEYWORDS" => "N",
-        		"SET_STATUS_404" => "N",
-        		"SET_TITLE" => "N",
-        		"SHOW_404" => "N",
-        		"SORT_BY1" => "ACTIVE_FROM",
-        		"SORT_BY2" => "SORT",
-        		"SORT_ORDER1" => "DESC",
-        		"SORT_ORDER2" => "ASC"
-        	)
-        ); ?>
-    </div>
-</section>
-<section class="why_us">
-    <div class="wrap wrap_why_us">
-        <strong class="heading">
-            <? $APPLICATION->IncludeComponent(
-            	"bitrix:main.include",
-            	"",
-            	Array(
-            		"AREA_FILE_SHOW" => "file",
-            		"PATH" => v::includedArea('homepage/why_us_heading.php')
-            	)
-            ); ?>
-        </strong>
-        <? $APPLICATION->IncludeComponent(
-        	"bitrix:news.list",
-        	"why_us",
-        	Array(
-        		"ACTIVE_DATE_FORMAT" => "j F Y",
-        		"ADD_SECTIONS_CHAIN" => "N",
-        		"AJAX_MODE" => "N",
-        		"AJAX_OPTION_ADDITIONAL" => "",
-        		"AJAX_OPTION_HISTORY" => "N",
-        		"AJAX_OPTION_JUMP" => "N",
-        		"AJAX_OPTION_STYLE" => "Y",
-        		"CACHE_FILTER" => "N",
-        		"CACHE_GROUPS" => "Y",
-        		"CACHE_TIME" => "36000000",
-        		"CACHE_TYPE" => "A",
-        		"CHECK_DATES" => "Y",
-        		"DETAIL_URL" => "",
-        		"DISPLAY_BOTTOM_PAGER" => "Y",
-        		"DISPLAY_DATE" => "Y",
-        		"DISPLAY_NAME" => "Y",
-        		"DISPLAY_PICTURE" => "Y",
-        		"DISPLAY_PREVIEW_TEXT" => "Y",
-        		"DISPLAY_TOP_PAGER" => "N",
-        		"FIELD_CODE" => array("", ""),
-        		"FILTER_NAME" => "",
-        		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-        		"IBLOCK_ID" => IblockTools::find(Iblock::CONTENT_TYPE, Iblock::WHY_US)->id(),
-        		"IBLOCK_TYPE" => Iblock::CONTENT_TYPE,
-        		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-        		"INCLUDE_SUBSECTIONS" => "Y",
-        		"MESSAGE_404" => "",
-        		"NEWS_COUNT" => PHP_INT_MAX,
-        		"PAGER_BASE_LINK_ENABLE" => "N",
-        		"PAGER_DESC_NUMBERING" => "N",
-        		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-        		"PAGER_SHOW_ALL" => "N",
-        		"PAGER_SHOW_ALWAYS" => "N",
-        		"PAGER_TEMPLATE" => ".default",
-        		"PAGER_TITLE" => '',
-        		"PARENT_SECTION" => "",
-        		"PARENT_SECTION_CODE" => "",
-        		"PREVIEW_TRUNCATE_LEN" => "",
-        		"PROPERTY_CODE" => array("ICON"),
-        		"SET_BROWSER_TITLE" => "N",
-        		"SET_LAST_MODIFIED" => "N",
-        		"SET_META_DESCRIPTION" => "N",
-        		"SET_META_KEYWORDS" => "N",
-        		"SET_STATUS_404" => "N",
-        		"SET_TITLE" => "N",
-        		"SHOW_404" => "N",
-        		"SORT_BY1" => "ACTIVE_FROM",
-        		"SORT_BY2" => "SORT",
-        		"SORT_ORDER1" => "DESC",
-        		"SORT_ORDER2" => "ASC"
-        	)
-        ); ?>
-    </div>
-</section>
-<div class="dots dots_why_us"></div>
+	<section class="service" data-anchor="next">
+		<div class="left wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0s"></div>
+		<div class="right wow fadeInRight" data-wow-duration="1s" data-wow-delay="0s"></div>
+		<div class="wrap">
+			<strong class="heading">
+				<? $APPLICATION->IncludeComponent(
+					"bitrix:main.include",
+					"",
+					Array(
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => v::includedArea('homepage/service_heading.php')
+					)
+				); ?>
+			</strong>
+			<? $APPLICATION->IncludeComponent(
+				"bitrix:news.list",
+				"service",
+				Array(
+					"ACTIVE_DATE_FORMAT" => "j F Y",
+					"ADD_SECTIONS_CHAIN" => "N",
+					"AJAX_MODE" => "N",
+					"AJAX_OPTION_ADDITIONAL" => "",
+					"AJAX_OPTION_HISTORY" => "N",
+					"AJAX_OPTION_JUMP" => "N",
+					"AJAX_OPTION_STYLE" => "Y",
+					"CACHE_FILTER" => "N",
+					"CACHE_GROUPS" => "Y",
+					"CACHE_TIME" => "36000000",
+					"CACHE_TYPE" => "A",
+					"CHECK_DATES" => "Y",
+					"DETAIL_URL" => "",
+					"DISPLAY_BOTTOM_PAGER" => "Y",
+					"DISPLAY_DATE" => "Y",
+					"DISPLAY_NAME" => "Y",
+					"DISPLAY_PICTURE" => "Y",
+					"DISPLAY_PREVIEW_TEXT" => "Y",
+					"DISPLAY_TOP_PAGER" => "N",
+					"FIELD_CODE" => array("", ""),
+					"FILTER_NAME" => "",
+					"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+					"IBLOCK_ID" => IblockTools::find(Iblock::CONTENT_TYPE, Iblock::SERVICE)->id(),
+					"IBLOCK_TYPE" => Iblock::CONTENT_TYPE,
+					"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+					"INCLUDE_SUBSECTIONS" => "Y",
+					"MESSAGE_404" => "",
+					"NEWS_COUNT" => PHP_INT_MAX,
+					"PAGER_BASE_LINK_ENABLE" => "N",
+					"PAGER_DESC_NUMBERING" => "N",
+					"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+					"PAGER_SHOW_ALL" => "N",
+					"PAGER_SHOW_ALWAYS" => "N",
+					"PAGER_TEMPLATE" => ".default",
+					"PAGER_TITLE" => '',
+					"PARENT_SECTION" => "",
+					"PARENT_SECTION_CODE" => "",
+					"PREVIEW_TRUNCATE_LEN" => "",
+					"PROPERTY_CODE" => array("LINK", "ICON"),
+					"SET_BROWSER_TITLE" => "N",
+					"SET_LAST_MODIFIED" => "N",
+					"SET_META_DESCRIPTION" => "N",
+					"SET_META_KEYWORDS" => "N",
+					"SET_STATUS_404" => "N",
+					"SET_TITLE" => "N",
+					"SHOW_404" => "N",
+					"SORT_BY1" => "ACTIVE_FROM",
+					"SORT_BY2" => "SORT",
+					"SORT_ORDER1" => "DESC",
+					"SORT_ORDER2" => "ASC"
+				)
+			); ?>
+		</div>
+	</section>
+	<section class="why_us">
+		<div class="wrap wrap_why_us">
+			<strong class="heading">
+				<? $APPLICATION->IncludeComponent(
+					"bitrix:main.include",
+					"",
+					Array(
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => v::includedArea('homepage/why_us_heading.php')
+					)
+				); ?>
+			</strong>
+			<? $APPLICATION->IncludeComponent(
+				"bitrix:news.list",
+				"why_us",
+				Array(
+					"ACTIVE_DATE_FORMAT" => "j F Y",
+					"ADD_SECTIONS_CHAIN" => "N",
+					"AJAX_MODE" => "N",
+					"AJAX_OPTION_ADDITIONAL" => "",
+					"AJAX_OPTION_HISTORY" => "N",
+					"AJAX_OPTION_JUMP" => "N",
+					"AJAX_OPTION_STYLE" => "Y",
+					"CACHE_FILTER" => "N",
+					"CACHE_GROUPS" => "Y",
+					"CACHE_TIME" => "36000000",
+					"CACHE_TYPE" => "A",
+					"CHECK_DATES" => "Y",
+					"DETAIL_URL" => "",
+					"DISPLAY_BOTTOM_PAGER" => "Y",
+					"DISPLAY_DATE" => "Y",
+					"DISPLAY_NAME" => "Y",
+					"DISPLAY_PICTURE" => "Y",
+					"DISPLAY_PREVIEW_TEXT" => "Y",
+					"DISPLAY_TOP_PAGER" => "N",
+					"FIELD_CODE" => array("", ""),
+					"FILTER_NAME" => "",
+					"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+					"IBLOCK_ID" => IblockTools::find(Iblock::CONTENT_TYPE, Iblock::WHY_US)->id(),
+					"IBLOCK_TYPE" => Iblock::CONTENT_TYPE,
+					"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+					"INCLUDE_SUBSECTIONS" => "Y",
+					"MESSAGE_404" => "",
+					"NEWS_COUNT" => PHP_INT_MAX,
+					"PAGER_BASE_LINK_ENABLE" => "N",
+					"PAGER_DESC_NUMBERING" => "N",
+					"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+					"PAGER_SHOW_ALL" => "N",
+					"PAGER_SHOW_ALWAYS" => "N",
+					"PAGER_TEMPLATE" => ".default",
+					"PAGER_TITLE" => '',
+					"PARENT_SECTION" => "",
+					"PARENT_SECTION_CODE" => "",
+					"PREVIEW_TRUNCATE_LEN" => "",
+					"PROPERTY_CODE" => array("ICON"),
+					"SET_BROWSER_TITLE" => "N",
+					"SET_LAST_MODIFIED" => "N",
+					"SET_META_DESCRIPTION" => "N",
+					"SET_META_KEYWORDS" => "N",
+					"SET_STATUS_404" => "N",
+					"SET_TITLE" => "N",
+					"SHOW_404" => "N",
+					"SORT_BY1" => "ACTIVE_FROM",
+					"SORT_BY2" => "SORT",
+					"SORT_ORDER1" => "DESC",
+					"SORT_ORDER2" => "ASC"
+				)
+			); ?>
+		</div>
+	</section>
+	<div class="dots dots_why_us"></div>
 <? // promotions ?>
-<section class="shares">
-    <div class="wrap">
-        <strong class="heading">
-            <? $APPLICATION->IncludeComponent(
-            	"bitrix:main.include",
-            	"",
-            	Array(
-            		"AREA_FILE_SHOW" => "file",
-            		"PATH" => v::includedArea('homepage/promotions_heading.php')
-            	)
-            ); ?>
-        </strong>
-        <? $APPLICATION->IncludeComponent(
-        	"bitrix:news.list",
-        	"promotions",
-        	Array(
-        		"ACTIVE_DATE_FORMAT" => "j F Y",
-        		"ADD_SECTIONS_CHAIN" => "N",
-        		"AJAX_MODE" => "N",
-        		"AJAX_OPTION_ADDITIONAL" => "",
-        		"AJAX_OPTION_HISTORY" => "N",
-        		"AJAX_OPTION_JUMP" => "N",
-        		"AJAX_OPTION_STYLE" => "Y",
-        		"CACHE_FILTER" => "N",
-        		"CACHE_GROUPS" => "Y",
-        		"CACHE_TIME" => "36000000",
-        		"CACHE_TYPE" => "A",
-        		"CHECK_DATES" => "Y",
-        		"DETAIL_URL" => "",
-        		"DISPLAY_BOTTOM_PAGER" => "Y",
-        		"DISPLAY_DATE" => "Y",
-        		"DISPLAY_NAME" => "Y",
-        		"DISPLAY_PICTURE" => "Y",
-        		"DISPLAY_PREVIEW_TEXT" => "Y",
-        		"DISPLAY_TOP_PAGER" => "N",
-        		"FIELD_CODE" => array("", ""),
-        		"FILTER_NAME" => "",
-        		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-        		"IBLOCK_ID" => IblockTools::find(Iblock::CONTENT_TYPE, Iblock::PROMOTIONS)->id(),
-        		"IBLOCK_TYPE" => Iblock::CONTENT_TYPE,
-        		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-        		"INCLUDE_SUBSECTIONS" => "Y",
-        		"MESSAGE_404" => "",
-        		"NEWS_COUNT" => PHP_INT_MAX,
-        		"PAGER_BASE_LINK_ENABLE" => "N",
-        		"PAGER_DESC_NUMBERING" => "N",
-        		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-        		"PAGER_SHOW_ALL" => "N",
-        		"PAGER_SHOW_ALWAYS" => "N",
-        		"PAGER_TEMPLATE" => ".default",
-        		"PAGER_TITLE" => '',
-        		"PARENT_SECTION" => "",
-        		"PARENT_SECTION_CODE" => "",
-        		"PREVIEW_TRUNCATE_LEN" => "",
-        		"PROPERTY_CODE" => array("", ""),
-        		"SET_BROWSER_TITLE" => "N",
-        		"SET_LAST_MODIFIED" => "N",
-        		"SET_META_DESCRIPTION" => "N",
-        		"SET_META_KEYWORDS" => "N",
-        		"SET_STATUS_404" => "N",
-        		"SET_TITLE" => "N",
-        		"SHOW_404" => "N",
-        		"SORT_BY1" => "ACTIVE_FROM",
-        		"SORT_BY2" => "SORT",
-        		"SORT_ORDER1" => "DESC",
-        		"SORT_ORDER2" => "ASC"
-        	)
-        ); ?>
-    </div>
-</section>
-<div class="dots dots_shares"></div>
-<section class="gallery">
-    <strong class="heading">
-        <? $APPLICATION->IncludeComponent(
-        	"bitrix:main.include",
-        	"",
-        	Array(
-        		"AREA_FILE_SHOW" => "file",
-        		"PATH" => v::includedArea('homepage/gallery_heading.php')
-        	)
-        ); ?>
-    </strong>
-    <? $APPLICATION->IncludeComponent(
-    	"bitrix:news.list",
-    	"gallery",
-    	Array(
-    		"ACTIVE_DATE_FORMAT" => "j F Y",
-    		"ADD_SECTIONS_CHAIN" => "N",
-    		"AJAX_MODE" => "N",
-    		"AJAX_OPTION_ADDITIONAL" => "",
-    		"AJAX_OPTION_HISTORY" => "N",
-    		"AJAX_OPTION_JUMP" => "N",
-    		"AJAX_OPTION_STYLE" => "Y",
-    		"CACHE_FILTER" => "N",
-    		"CACHE_GROUPS" => "Y",
-    		"CACHE_TIME" => "36000000",
-    		"CACHE_TYPE" => "A",
-    		"CHECK_DATES" => "Y",
-    		"DETAIL_URL" => "",
-    		"DISPLAY_BOTTOM_PAGER" => "Y",
-    		"DISPLAY_DATE" => "Y",
-    		"DISPLAY_NAME" => "Y",
-    		"DISPLAY_PICTURE" => "Y",
-    		"DISPLAY_PREVIEW_TEXT" => "Y",
-    		"DISPLAY_TOP_PAGER" => "N",
-    		"FIELD_CODE" => array("", ""),
-    		"FILTER_NAME" => "",
-    		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-    		"IBLOCK_ID" => IblockTools::find(Iblock::CONTENT_TYPE, Iblock::GALLERY)->id(),
-    		"IBLOCK_TYPE" => Iblock::CONTENT_TYPE,
-    		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-    		"INCLUDE_SUBSECTIONS" => "Y",
-    		"MESSAGE_404" => "",
-    		"NEWS_COUNT" => PHP_INT_MAX,
-    		"PAGER_BASE_LINK_ENABLE" => "N",
-    		"PAGER_DESC_NUMBERING" => "N",
-    		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-    		"PAGER_SHOW_ALL" => "N",
-    		"PAGER_SHOW_ALWAYS" => "N",
-    		"PAGER_TEMPLATE" => ".default",
-    		"PAGER_TITLE" => '',
-    		"PARENT_SECTION" => "",
-    		"PARENT_SECTION_CODE" => "",
-    		"PREVIEW_TRUNCATE_LEN" => "",
-    		"PROPERTY_CODE" => array("", ""),
-    		"SET_BROWSER_TITLE" => "N",
-    		"SET_LAST_MODIFIED" => "N",
-    		"SET_META_DESCRIPTION" => "N",
-    		"SET_META_KEYWORDS" => "N",
-    		"SET_STATUS_404" => "N",
-    		"SET_TITLE" => "N",
-    		"SHOW_404" => "N",
-    		"SORT_BY1" => "ACTIVE_FROM",
-    		"SORT_BY2" => "SORT",
-    		"SORT_ORDER1" => "DESC",
-    		"SORT_ORDER2" => "ASC"
-    	)
-    ); ?>
-</section>
-<div class="dots dots_gallery"></div>
+	<section class="shares">
+		<div class="wrap">
+			<strong class="heading">
+				<? $APPLICATION->IncludeComponent(
+					"bitrix:main.include",
+					"",
+					Array(
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => v::includedArea('homepage/promotions_heading.php')
+					)
+				); ?>
+			</strong>
+			<? $APPLICATION->IncludeComponent(
+				"bitrix:news.list",
+				"promotions",
+				Array(
+					"ACTIVE_DATE_FORMAT" => "j F Y",
+					"ADD_SECTIONS_CHAIN" => "N",
+					"AJAX_MODE" => "N",
+					"AJAX_OPTION_ADDITIONAL" => "",
+					"AJAX_OPTION_HISTORY" => "N",
+					"AJAX_OPTION_JUMP" => "N",
+					"AJAX_OPTION_STYLE" => "Y",
+					"CACHE_FILTER" => "N",
+					"CACHE_GROUPS" => "Y",
+					"CACHE_TIME" => "36000000",
+					"CACHE_TYPE" => "A",
+					"CHECK_DATES" => "Y",
+					"DETAIL_URL" => "",
+					"DISPLAY_BOTTOM_PAGER" => "Y",
+					"DISPLAY_DATE" => "Y",
+					"DISPLAY_NAME" => "Y",
+					"DISPLAY_PICTURE" => "Y",
+					"DISPLAY_PREVIEW_TEXT" => "Y",
+					"DISPLAY_TOP_PAGER" => "N",
+					"FIELD_CODE" => array("", ""),
+					"FILTER_NAME" => "",
+					"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+					"IBLOCK_ID" => IblockTools::find(Iblock::CONTENT_TYPE, Iblock::PROMOTIONS)->id(),
+					"IBLOCK_TYPE" => Iblock::CONTENT_TYPE,
+					"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+					"INCLUDE_SUBSECTIONS" => "Y",
+					"MESSAGE_404" => "",
+					"NEWS_COUNT" => PHP_INT_MAX,
+					"PAGER_BASE_LINK_ENABLE" => "N",
+					"PAGER_DESC_NUMBERING" => "N",
+					"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+					"PAGER_SHOW_ALL" => "N",
+					"PAGER_SHOW_ALWAYS" => "N",
+					"PAGER_TEMPLATE" => ".default",
+					"PAGER_TITLE" => '',
+					"PARENT_SECTION" => "",
+					"PARENT_SECTION_CODE" => "",
+					"PREVIEW_TRUNCATE_LEN" => "",
+					"PROPERTY_CODE" => array("", ""),
+					"SET_BROWSER_TITLE" => "N",
+					"SET_LAST_MODIFIED" => "N",
+					"SET_META_DESCRIPTION" => "N",
+					"SET_META_KEYWORDS" => "N",
+					"SET_STATUS_404" => "N",
+					"SET_TITLE" => "N",
+					"SHOW_404" => "N",
+					"SORT_BY1" => "ACTIVE_FROM",
+					"SORT_BY2" => "SORT",
+					"SORT_ORDER1" => "DESC",
+					"SORT_ORDER2" => "ASC"
+				)
+			); ?>
+		</div>
+	</section>
+	<div class="dots dots_shares"></div>
+	<section class="gallery">
+		<strong class="heading">
+			<? $APPLICATION->IncludeComponent(
+				"bitrix:main.include",
+				"",
+				Array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => v::includedArea('homepage/gallery_heading.php')
+				)
+			); ?>
+		</strong>
+		<? $APPLICATION->IncludeComponent(
+			"bitrix:news.list",
+			"gallery",
+			Array(
+				"ACTIVE_DATE_FORMAT" => "j F Y",
+				"ADD_SECTIONS_CHAIN" => "N",
+				"AJAX_MODE" => "N",
+				"AJAX_OPTION_ADDITIONAL" => "",
+				"AJAX_OPTION_HISTORY" => "N",
+				"AJAX_OPTION_JUMP" => "N",
+				"AJAX_OPTION_STYLE" => "Y",
+				"CACHE_FILTER" => "N",
+				"CACHE_GROUPS" => "Y",
+				"CACHE_TIME" => "36000000",
+				"CACHE_TYPE" => "A",
+				"CHECK_DATES" => "Y",
+				"DETAIL_URL" => "",
+				"DISPLAY_BOTTOM_PAGER" => "Y",
+				"DISPLAY_DATE" => "Y",
+				"DISPLAY_NAME" => "Y",
+				"DISPLAY_PICTURE" => "Y",
+				"DISPLAY_PREVIEW_TEXT" => "Y",
+				"DISPLAY_TOP_PAGER" => "N",
+				"FIELD_CODE" => array("", ""),
+				"FILTER_NAME" => "",
+				"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+				"IBLOCK_ID" => IblockTools::find(Iblock::CONTENT_TYPE, Iblock::GALLERY)->id(),
+				"IBLOCK_TYPE" => Iblock::CONTENT_TYPE,
+				"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+				"INCLUDE_SUBSECTIONS" => "Y",
+				"MESSAGE_404" => "",
+				"NEWS_COUNT" => PHP_INT_MAX,
+				"PAGER_BASE_LINK_ENABLE" => "N",
+				"PAGER_DESC_NUMBERING" => "N",
+				"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+				"PAGER_SHOW_ALL" => "N",
+				"PAGER_SHOW_ALWAYS" => "N",
+				"PAGER_TEMPLATE" => ".default",
+				"PAGER_TITLE" => '',
+				"PARENT_SECTION" => "",
+				"PARENT_SECTION_CODE" => "",
+				"PREVIEW_TRUNCATE_LEN" => "",
+				"PROPERTY_CODE" => array("", ""),
+				"SET_BROWSER_TITLE" => "N",
+				"SET_LAST_MODIFIED" => "N",
+				"SET_META_DESCRIPTION" => "N",
+				"SET_META_KEYWORDS" => "N",
+				"SET_STATUS_404" => "N",
+				"SET_TITLE" => "N",
+				"SHOW_404" => "N",
+				"SORT_BY1" => "ACTIVE_FROM",
+				"SORT_BY2" => "SORT",
+				"SORT_ORDER1" => "DESC",
+				"SORT_ORDER2" => "ASC"
+			)
+		); ?>
+	</section>
+	<div class="dots dots_gallery"></div>
 <? $APPLICATION->IncludeComponent(
 	"bitrix:news.list",
 	"banners_section",
