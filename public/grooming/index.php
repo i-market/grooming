c2f6385f8f60f2b7e3c478e54235acdea3169449
Artem Novikov <novikov@i-market.ru>
2017-04-11 19:32:23 +0300

    grooming gallery section

--- public/grooming/index.php
+++ public/grooming/index.php
@@ -84,35 +84,72 @@ $APPLICATION->SetPageProperty(PageProperty::LAYOUT, ['base.twig', App::layoutCon
     </div>
 </section>
 <section class="before_after">
-    <strong class="heading"><h2>Галерея</h2></strong>
-    <div class="grid gallery_big">
-        <a href="images/pic_12.jpg" rel="gallery" class="gellery_item col col_5">
-            <img src="images/pic_12.jpg" alt="">
-        </a>
-        <a href="images/pic_12.jpg" rel="gallery" class="gellery_item col col_5">
-            <img src="images/pic_12.jpg" alt="">
-        </a>
-        <a href="images/pic_12.jpg" rel="gallery" class="gellery_item col col_5">
-            <img src="images/pic_12.jpg" alt="">
-        </a>
-        <a href="images/pic_12.jpg" rel="gallery" class="gellery_item col col_5">
-            <img src="images/pic_12.jpg" alt="">
-        </a>
-        <a href="images/pic_12.jpg" rel="gallery" class="gellery_item col col_5">
-            <img src="images/pic_12.jpg" alt="">
-        </a>
-        <a href="images/pic_12.jpg" rel="gallery" class="gellery_item col col_5">
-            <img src="images/pic_12.jpg" alt="">
-        </a>
-        <a href="images/pic_12.jpg" rel="gallery" class="gellery_item col col_5">
-            <img src="images/pic_12.jpg" alt="">
-        </a>
-    </div>
-    <div class="gallery_big_controls">
-        <span class="prev"></span>
-        <div class="dots gallery_big_dots"></div>
-        <span class="next"></span>
-    </div>
+    <strong class="heading">
+        <? $APPLICATION->IncludeComponent(
+        	"bitrix:main.include",
+        	"",
+        	Array(
+        		"AREA_FILE_SHOW" => "file",
+        		"PATH" => v::includedArea('grooming/gallery_header.php')
+        	)
+        ); ?>
+    </strong>
+    <? $APPLICATION->IncludeComponent(
+    	"bitrix:news.list",
+    	"grooming_gallery",
+    	Array(
+    		"ACTIVE_DATE_FORMAT" => "j F Y",
+    		"ADD_SECTIONS_CHAIN" => "N",
+    		"AJAX_MODE" => "N",
+    		"AJAX_OPTION_ADDITIONAL" => "",
+    		"AJAX_OPTION_HISTORY" => "N",
+    		"AJAX_OPTION_JUMP" => "N",
+    		"AJAX_OPTION_STYLE" => "Y",
+    		"CACHE_FILTER" => "N",
+    		"CACHE_GROUPS" => "Y",
+    		"CACHE_TIME" => "36000000",
+    		"CACHE_TYPE" => "A",
+    		"CHECK_DATES" => "Y",
+    		"DETAIL_URL" => "",
+    		"DISPLAY_BOTTOM_PAGER" => "Y",
+    		"DISPLAY_DATE" => "Y",
+    		"DISPLAY_NAME" => "Y",
+    		"DISPLAY_PICTURE" => "Y",
+    		"DISPLAY_PREVIEW_TEXT" => "Y",
+    		"DISPLAY_TOP_PAGER" => "N",
+    		"FIELD_CODE" => array("DETAIL_PICTURE"),
+    		"FILTER_NAME" => "",
+    		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+    		"IBLOCK_ID" => IblockTools::find(Iblock::CONTENT_TYPE, Iblock::IMAGES)->id(),
+    		"IBLOCK_TYPE" => Iblock::CONTENT_TYPE,
+    		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+    		"INCLUDE_SUBSECTIONS" => "Y",
+    		"MESSAGE_404" => "",
+    		"NEWS_COUNT" => PHP_INT_MAX,
+    		"PAGER_BASE_LINK_ENABLE" => "N",
+    		"PAGER_DESC_NUMBERING" => "N",
+    		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+    		"PAGER_SHOW_ALL" => "N",
+    		"PAGER_SHOW_ALWAYS" => "N",
+    		"PAGER_TEMPLATE" => ".default",
+    		"PAGER_TITLE" => '',
+    		"PARENT_SECTION" => "",
+    		"PARENT_SECTION_CODE" => "grooming_gallery",
+    		"PREVIEW_TRUNCATE_LEN" => "",
+    		"PROPERTY_CODE" => array("", ""),
+    		"SET_BROWSER_TITLE" => "N",
+    		"SET_LAST_MODIFIED" => "N",
+    		"SET_META_DESCRIPTION" => "N",
+    		"SET_META_KEYWORDS" => "N",
+    		"SET_STATUS_404" => "N",
+    		"SET_TITLE" => "N",
+    		"SHOW_404" => "N",
+    		"SORT_BY1" => "ACTIVE_FROM",
+    		"SORT_BY2" => "SORT",
+    		"SORT_ORDER1" => "DESC",
+    		"SORT_ORDER2" => "ASC"
+    	)
+    ); ?>
 </section>
 <section class="why_us why_us--pages">
     <div class="wrap wrap_why_us">
