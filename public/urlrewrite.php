c51b4d99f820ed3ad0b36ff7fd138ec8b687185b
Artem Novikov <novikov@i-market.ru>
2017-04-21 13:22:06 +0300

    urlrewrite grooming/services

--- public/urlrewrite.php
+++ public/urlrewrite.php
@@ -1,6 +1,12 @@
 <?
 $arUrlRewrite = array(
-	array(
+    array(
+        "CONDITION" => "#^/grooming/([a-zA-Z_-]+)/([a-zA-Z_-]+)/#",
+        "RULE" => "SECTION_CODE=\$1&IBLOCK_CODE=\$2",
+        "ID" => "",
+        "PATH" => "/grooming/services.php",
+    ),
+    array(
 		"CONDITION" => "#^/api/.*#",
 		"RULE" => "",
 		"ID" => "",
