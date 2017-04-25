5ea75e0430c5a14ebcd67de5c02c332650641901
Artem Novikov <novikov@i-market.ru>
2017-04-25 13:27:53 +0300

    add body parts migration

---
+++ public/admin/migrations/3-add-body-part-elements.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Iblock;
+use Bex\Tools\Iblock\IblockTools;
+use Core\Nullable as nil;
+use Core\Underscore as _;
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("migration");
+
+$tasks = nil::get(nil::map($_REQUEST['tasks'], function($s) {
+    return explode(',', $s);
+}), []);
+if ($USER->IsAdmin() && !_::isEmpty($tasks)) {
+    $results = [];
+    if (in_array('default', $tasks)) {
+        $previewText = 'Красота и здоровье Вашего питомца – основные принципы нашей работы, поэтому мы выбираем только качественную и прошедшую сертификацию косметику. Красота и здоровье Вашего питомца – основные принципы нашей работы, поэтому мы выбираем.';
+        foreach (range(1, 8) as $n) {
+            $el = new CIBlockElement();
+            $result = $el->Add([
+                'IBLOCK_ID' => IblockTools::find(Iblock::CONTENT_TYPE, Iblock::BODY_PARTS)->id(),
+                'NAME' => 'Часть тела '.$n,
+                'PREVIEW_TEXT' => $previewText,
+                'PROPERTY_VALUES' => [
+                    'MARKER_ID' => 'plus'.$n
+                ]
+            ]);
+            $results[] = [$result, $el->LAST_ERROR];
+        }
+    }
+    var_export($results);
+} else {
+    echo 'did you forget something?';
+}
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
