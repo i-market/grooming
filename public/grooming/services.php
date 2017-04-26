69339b4cab424d3b95589113b079a40065026737
Artem Novikov <novikov@i-market.ru>
2017-04-26 16:02:20 +0300

    sort service table rows by creation date

--- public/grooming/services.php
+++ public/grooming/services.php
@@ -60,9 +60,9 @@ $APPLICATION->SetPageProperty(PageProperty::LAYOUT, 'base.twig');
 		"SET_STATUS_404" => "N",
 		"SET_TITLE" => "Y",
 		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY1" => "DATE_CREATE",
 		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER1" => "ASC",
 		"SORT_ORDER2" => "ASC"
 	)
 ); ?>
