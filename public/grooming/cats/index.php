97d64bf4d7e3df7c1408bda167dff9ab7b03de55
Artem Novikov <novikov@i-market.ru>
2017-09-13 14:34:40 +0300

    pull from staging: bitrix editor bad file formatting

--- public/grooming/cats/index.php
+++ public/grooming/cats/index.php
@@ -12,14 +12,12 @@ $APPLICATION->SetPageProperty(PageProperty::LAYOUT, ['base.twig', function() {
     ]);
 }]);
 $sectionCode = 'cats';
-?>
-
-<section class="services_for_dogs" data-anchor="next">
-    <div class="wrap">
-        <strong class="heading"><h2><? $APPLICATION->ShowTitle() ?></h2></strong>
-        <?= Services::renderServiceTypesGrid($sectionCode) ?>
-    </div>
-</section>
-<?= Services::renderGallerySection($sectionCode) ?>
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><section class="services_for_dogs" data-anchor="next">
+<div class="wrap">
+ <strong class="heading">
+	<h2><? $APPLICATION->ShowTitle() ?></h2>
+ </strong>
+	<?= Services::renderServiceTypesGrid($sectionCode) ?>
+</div>
+ </section>
+<?= Services::renderGallerySection($sectionCode) ?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
