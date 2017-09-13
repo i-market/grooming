936ca3e0a18c3fdf7b68fd0d40b2b034269d2680
Artem Novikov <novikov@i-market.ru>
2017-09-13 14:36:00 +0300

    pull from prod: add a stub page in /promotions/

---
+++ public/promotions/aktsiya.php
@@ -0,0 +1,7 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("tags", "Акция");
+$APPLICATION->SetPageProperty("keywords", "Акция");
+$APPLICATION->SetPageProperty("description", "Акция");
+$APPLICATION->SetTitle("Акция");
+?>Текст акции&nbsp;<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
